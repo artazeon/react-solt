c1940fc90b782f599da8d5c1de5f249422740a61
artazeon <artazeon@gmail.com>
2026-03-01 10:36:56 +0300

    Responsive

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './styles/variables.scss'
 import './styles/_buttons.scss'
+import './styles/normalize.scss'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
